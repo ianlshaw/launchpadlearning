c920fc38251fef6293d3ab47dc77d0e513f674f5
Ian Shaw <ian.shaw@thirtythree.co.uk>
2016-03-29 15:23:33 +0100

    printed strings have been made into CONSTANTS

--- learnlp.rb
+++ learnlp.rb
@@ -12,22 +12,28 @@ TEMPO = 1
 
 BLANK_FRAME_DEFAULT = " "
 
-
+# These are the strings which will be printed to the terminal
+HIT = "[*]"
+BLANK = "[ ]"
 
 # Variables
-# Each frame is a hash of active coordinates.
-@fr1 = Hash["a1" => "[*]", "a2" => "[*]"]
-@fr1.default = "[ ]"
+# Each frame is a hash of ac://monitor.thirtythreebuild.co.uk/icinga/cgi-bin/tac.cgitive coordinates.
+@frame1 = Hash["a1" => HIT, "a2" => HIT, "h7" => HIT]
+
+# This can maybe be autopopulated once we have a list of frames.
+@frame1.default = BLANK
+
+@frame2 = Hash["b4" => HIT, "c8" => HIT]
+@frame2.default = BLANK
 
 # list of all the frames. 
 @frames = ""
 
 # Functions
-
 def drawBoard
       ('a'..'h').each do |letter|
         (1..8).each do |i|
-        print @fr1["#{letter}#{i}"]
+        print @frame1["#{letter}#{i}"]
         end
       puts  # end the line
       end
